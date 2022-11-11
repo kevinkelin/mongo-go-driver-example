5f2a25f43d4177a2dbca88ac4f74c9edd037a99c
yangyanxing <545239288@qq.com>
2022-11-11 15:49:15 +0800

    添加几个查询与更新的函数

--- example/query.go
+++ example/query.go
@@ -1,1 +1,51 @@
 package example
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func QueryOneByNothing(ctx context.Context, db *mongo.Database) error {
+	/*
+		没有任何搜索条件， 但是也要写一个, 默认会查询数据库中的第一条记录
+	*/
+	c := db.Collection("yyxtest")
+	result := c.FindOne(ctx, bson.M{})
+	var data map[string]interface{}
+	err := result.Decode(&data)
+	fmt.Println(data)
+	return err
+}
+
+func QueryOneByNothingDesc(ctx context.Context, db *mongo.Database) error {
+	/*
+		倒序返回最后一个, 使用_id 进行排序
+	*/
+	c := db.Collection("yyxtest")
+	findOpt := options.FindOneOptions{Sort: bson.M{"_id": -1}}
+	result := c.FindOne(ctx, bson.M{}, &findOpt)
+	var data map[string]interface{}
+	err := result.Decode(&data)
+	fmt.Println(data)
+	return err
+}
+
+func QueryOneByNothingMulSort(ctx context.Context, db *mongo.Database) error {
+	/*
+		倒序返回最后一个, 这里使用多个排序因子，这里就要使用bson.D了，先按name升序，再按age做降序
+	*/
+	c := db.Collection("yyxtest")
+	findOpt := options.FindOneOptions{Sort: bson.D{
+		{Key: "name", Value: 1},
+		{Key: "age", Value: -1},
+	}}
+	result := c.FindOne(ctx, bson.M{}, &findOpt)
+	var data map[string]interface{}
+	err := result.Decode(&data)
+	fmt.Println(data)
+	return err
+}
--- example/update.go
+++ example/update.go
@@ -1,1 +1,41 @@
 package example
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func UpdateDocOrInsert(ctx context.Context, db *mongo.Database) error {
+	/*
+		更新文档，如果没有则新添加
+		ModifiedCount, 如果没有更新的，比如说要更新的内容和搜索出来的文件是一致的，则不更新
+	*/
+	c := db.Collection("yyxtest")
+	filter := bson.M{
+		"name": "lisi",
+		// "age":  20,
+	}
+	updates := bson.M{
+		"$set": bson.M{
+			"age": 21,
+		},
+	}
+	upsert := true
+	ops := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	result, err := c.UpdateOne(ctx, filter, updates, &ops)
+	if err != nil {
+		return err
+	}
+	fmt.Println("MatchedCount:", result.MatchedCount)
+	fmt.Println("ModifiedCount:", result.ModifiedCount)
+	fmt.Println("UpsertedCount:", result.UpsertedCount)
+	return nil
+
+}
